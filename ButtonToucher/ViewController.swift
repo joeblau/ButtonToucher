bcd8bd7ad3720c7b05d3ab3c69c22d8a6d5b4d29
Joe Blau <josephblau@gmail.com>
2019-01-05 21:00:46 -0500

    🤯

--- ButtonToucher/ViewController.swift
+++ ButtonToucher/ViewController.swift
@@ -10,11 +10,46 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let oneButton: UIButton
+    let twoButton: UIButton
+    
+    init() {
+        oneButton = UIButton(type: .system)
+        twoButton = UIButton(type: .system)
+        super.init(nibName: nil, bundle: nil)
+        oneButton.setTitle("Edit", for: .normal)
+        oneButton.setImage(#imageLiteral(resourceName: "edit"), for: .normal)
+        oneButton.tintColor = .black
+        oneButton.addTarget(self, action: #selector(oneButtonPressed(sender:)), for: .touchUpInside)
+        oneButton.sizeToFit()
+        
+        twoButton.setTitle("Favorite", for: .normal)
+        twoButton.setImage(#imageLiteral(resourceName: "favorite"), for: .normal)
+        twoButton.tintColor = .black
+        twoButton.addTarget(self, action: #selector(twoButtonPressed(sender:)), for: .touchUpInside)
+        twoButton.sizeToFit()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        view.backgroundColor = .white
+        view.addSubview(oneButton)
+        oneButton.center = CGPoint(x: view.center.x, y: view.center.y - 100)
+    
+        view.addSubview(twoButton)
+        twoButton.center = CGPoint(x: view.center.x, y: view.center.y + 100)
     }
-
-
+    
+    @objc func oneButtonPressed(sender: UIButton) {
+        print(sender)
+    }
+    
+    @objc func twoButtonPressed(sender: UIButton) {
+        print(sender)
+    }
 }
 
